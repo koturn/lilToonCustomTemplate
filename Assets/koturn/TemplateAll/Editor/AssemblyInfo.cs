abcba078934eb05c39d46b919ff7e6db0fc0f5cc
koturn <jeak.koutan.apple@gmail.com>
2024-12-13 09:22:28 +0900

    chore: Add comments

--- Assets/koturn/TemplateAll/Editor/AssemblyInfo.cs
+++ Assets/koturn/TemplateAll/Editor/AssemblyInfo.cs
@@ -1,12 +1,27 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-// [assembly: AssemblyCompany("COMPANY")]
-[assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCopyright("Copyright (C) 2024 YOURNAME All Rights Reserverd.")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TemplateAll")]
 [assembly: AssemblyDescription("Material inspector of TemplateAll.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("koturn")]
+[assembly: AssemblyProduct("TemplateAll")]
+[assembly: AssemblyCopyright("Copyright (C) 2024 YOURNAME All Rights Reserverd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
-[assembly: AssemblyProduct("TemplateAll")]
-[assembly: AssemblyTitle("TemplateAll")]
+
+// Metadata
 // [assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/YOURNAME/REPOSITORY")]
