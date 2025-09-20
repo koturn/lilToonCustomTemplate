1533a0f5b8854d8c4d79dea994845661f5679f2b
koturn <jeak.koutan.apple@gmail.com>
2025-09-20 18:29:04 +0900

    chore: Add uniform variable declaration for new VRChat feature and TVPro texture as comment

--- Assets/koturn/TemplateAll/Shaders/custom_insert.hlsl
+++ Assets/koturn/TemplateAll/Shaders/custom_insert.hlsl
@@ -4,16 +4,31 @@
 // float4 _AudioTexture_TexelSize;
 // #endif  // !defined(LIL_FEATURE_AUDIOLINK) && !defined(AUDIOLINK_CGINC_INCLUDED)
 
+// ProTV texture.
+// TEXTURE2D(_Udon_VideoTex);
+// float4 _Udon_VideoTex_TexelSize;
+
 // 0: Rendering normally
 // 1: Rendering in VR handheld camera
 // 2: Rendering in Desktop handheld camera
 // 3: Rendering for a screenshot
-// float _VRChatCameraMode
+// float _VRChatCameraMode;
 
 // 0: Rendering normally, not in a mirror
 // 1: Rendering in a mirror viewed in VR
 // 2: Rendering in a mirror viewed in desktop mode
-// float _VRChatMirrorMode
+// float _VRChatMirrorMode;
 
 // World space position of mirror camera (eye independent, "centered" in VR)
-// float _VRChatMirrorCameraPos
+// (0,0,0) when not rendering in a mirror.
+// float _VRChatMirrorCameraPos;
+
+// World space position of main screen camera.
+// float3 _VRChatScreenCameraPos;
+// World space rotation (quaternion) of main screen camera.
+// float4 _VRChatScreenCameraRot;
+
+// World space position of handheld photo camera (first instance when using Dolly Multicam), (0,0,0) when camera is not active.
+// float3 _VRChatPhotoCameraPos;
+// World space rotation (quaternion) of photo camera.
+// float4 _VRChatPhotoCameraRot;
